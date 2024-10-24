683820733f61aa44a33ab52499cf6d8cffef465f
Adam-Paskevic <adampaskevic@gmail.com>
2024-10-24 12:34:54 +0200

    kostra

---
+++ src/app/profile/page.tsx
@@ -0,0 +1,16 @@
+import Typography from "@mui/material/Typography";
+
+export const metadata = { title: "Profil" };
+
+export default function Home() {
+
+  return (
+    
+      <Typography> Profile </Typography>
+
+
+
+
+      
+  );
+}
